ba27935945989b38a13d611f180a2cf66ce65a77
Saikat Sarkar <saiksark@cisco.com>
2019-07-06 22:28:25 -0600

    Configure devise to show sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'page/index'
-  root 'page#index'
+  # root 'page#index'
   post 'page/broadcast'
+
+  resources :users, only: [:index]
+  resources :personal_messages, only: [:new, :create]
+  resources :conversations, only: [:index, :show]
+
+  # mount ActionCable.server => '/cable'
+
+  #root 'conversations#index'
+  root 'users#index'
+  #root 'devise/registrations#new'
+  #root 'devise/sessions#destroy' 
 end
