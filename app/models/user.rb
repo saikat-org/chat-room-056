c43974e205689c5eadac449ec1f78351f66a62d5
Saikat Sarkar <saiksark@cisco.com>
2019-07-07 23:04:53 -0600

    Send personal message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+
+  has_many :personal_messages, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_043629) do
+ActiveRecord::Schema.define(version: 2019_07_08_045856) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "message_id"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2019_07_04_043629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "personal_messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_personal_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_personal_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
