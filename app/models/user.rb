0de40342fda5c0e7d1247562f4615073116d5133
Saikat Sarkar <saiksark@cisco.com>
2019-07-18 15:55:54 -0600

    Show personal conversation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
   has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
-
   has_many :personal_messages, dependent: :destroy
 end
