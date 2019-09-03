75ef00f5c43c169478bc1a436b818b16e4c84899
Saikat Sarkar <saiksark@cisco.com>
2019-09-03 13:25:33 -0600

    Add attachment for chat room

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  def emojify(content)
-    content.to_str.gsub(/:([\w+-]+):/) do |match|
-      if emoji = Emoji.find_by_alias($1)
-        %(<img alt="#$1" src="/images/emoji/#{emoji.image_filename}" style="vertical-align:middle" width="20" height="20" />)
-      else
-        match
-      end
-    end.html_safe if content.present?
-  end
-
   private
   def configure_permitted_parameters
     added_attrs = [:first_name, :last_name, :email, :password, :password_confirmation, :avatar]
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,7 @@
 class Conversation < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
-
   has_many :private_conversations, -> { order(created_at: :asc)  }, dependent: :destroy
-
   validates :author, uniqueness: {scope: :receiver}
 
   scope :participating, -> (user) do
