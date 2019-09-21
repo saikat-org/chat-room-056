311ab0494480582ad578ae8c2f4335f259806898
Saikat Sarkar <saiksark@cisco.com>
2019-09-21 17:50:50 -0600

    Add spec for User model

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :user do
+    first_name 'first'
+    last_name  'last'
+    email "abc@account.com"
+    password '123456'
+
+    factory :user_with_avatar, class: User do
+      after(:build) do |user|
+        user.avatar.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'avatar.jpg')), filename: 'avatar.jpg', content_type: 'image/jpeg')
+      end
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'generates a valid user' do
-    expect{User.create(email: "abc@account.com", password: '123456')}.to change {User.count}.by(1)
+  let(:first_name)  {"first"}
+  let(:last_name)   {"last"}
+  subject { build(:user, first_name: first_name, last_name: last_name)}
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+    
+    it "is not valid without email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe "Associations" do
+    it "has many authored_conversations" do
+      assc = described_class.reflect_on_association(:authored_conversations)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it "has many received_conversations" do
+      assc = described_class.reflect_on_association(:received_conversations)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it "has many private_conversations" do
+      assc = described_class.reflect_on_association(:private_conversations)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it "can attach an avatar" do
+      user_with_avatar =  create(:user_with_avatar)
+      expect(user_with_avatar.avatar).to be_attached 
+    end
+  end
+
+  describe "#full_name" do
+    it "returns capitalized full name" do
+      expect(subject.full_name).to include(first_name.capitalize)
+      expect(subject.full_name).to include(last_name.capitalize)
+    end
   end
 end
