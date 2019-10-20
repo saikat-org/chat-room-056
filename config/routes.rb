b1edaa428ba97a3fd69e4d74495af7a2eca4e18c
Saikat Sarkar <saiksark@cisco.com>
2019-10-20 01:26:52 -0600

    Add spec for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'page/index'
-  # root 'page#index'
   post 'page/broadcast'
 
   resources :users, only: [:index]
   resources :private_conversations, only: [:new, :create]
   resources :conversations, only: [:index, :show]
 
-  # mount ActionCable.server => '/cable'
-
-  #root 'conversations#index'
   root 'users#index'
   #root 'devise/registrations#new'
   #root 'devise/sessions#destroy' 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-johnny = User.create email: "johnny@account.com", first_name: 'johnny', last_name: 'depp', encrypted_password: '123'
-saikat = User.create email: "saikat@account.com", first_name: 'saikat', last_name: 'sarkar', encrypted_password: '123'
-sumit  = User.create email: "sumit@account.com", first_name: 'sumit', last_name: 'sarkar', encrypted_password: '123'
+johnny = User.create email: "johnny@account.com", first_name: 'johnny', last_name: 'depp', encrypted_password: '123', password: '12345678'
+saikat = User.create email: "saikat@account.com", first_name: 'saikat', last_name: 'sarkar', encrypted_password: '123', password: '12345678'
+sumit  = User.create email: "sumit@account.com", first_name: 'sumit', last_name: 'sarkar', encrypted_password: '123', password: '12345678'
 
 Message.create title: "This is a message from John",   user: johnny
 Message.create title: "This is a message from Saikat", user: saikat
